a064255ed004f09a70d54fa68fc781bcf0992361
chande333 <benavides.silva@gmail.com>
2022-11-29 14:48:18 +0100

    Update fetchs.js

--- fetchs.js
+++ fetchs.js
@@ -11,3 +11,21 @@ fetch(test_url, {
     .then(data => {
         console.log(data);
     })
+
+
+// Used in Run.JS to test a specific function within a PHP
+function pingURL(testurl,fnName){
+ 
+  fetch(testurl, {
+            "headers": {
+                "accept": "*/*",
+                "accept-language": "en-US,en;q=0.9"
+            },
+            "body": `{"fn":"${fnName}"}`,
+  "method": "POST",
+        }).then(response=>response.text())
+        .then(data=>{
+            console.log(data);
+           return data;
+        })
+}
